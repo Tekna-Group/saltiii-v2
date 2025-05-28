f411c9a537c15be8accd483b254af4093debb948
Renz Christian Cabato <renzcabato@Renzs-Mac-mini.local>
2025-05-28 22:42:20 +0800

    Done

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('projects', 'Api\ProjectController');
+Route::patch('projects/{id}/complete', 'Api\ProjectController@complete');
+Route::post('projects/{id}/assign-users', 'Api\ProjectController@assignUsers');
+Route::post('projects/{id}/assign-team', 'Api\ProjectController@assignTeam');
+
+Route::apiResource('tasks', 'Api\TaskController');
+Route::patch('tasks/{id}/complete', 'Api\TaskController@complete');
+Route::patch('tasks/{id}/deadline', 'Api\TaskController@setDeadline');
+Route::post('tasks/{id}/assign-users', 'Api\TaskController@assignUsers');
+Route::post('tasks/{id}/comments', 'Api\TaskController@addComment');
+Route::post('tasks/{id}/attachments', 'Api\TaskController@uploadAttachment');
+Route::post('tasks/{id}/activity', 'Api\TaskController@logActivity');
+
