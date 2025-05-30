be5ec29678191b7fbae04b91dd5eb2544fbca884
Renz Christian Cabato <renzcabato@Renzs-Mac-mini.local>
2025-05-30 23:45:28 +0800

    Done

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::redirect('/', '/api/documentation');
 Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
     
