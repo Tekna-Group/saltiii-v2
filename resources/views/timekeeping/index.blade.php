b50fba027c9039a7112c7aaa2608a96b85b44966
Renz Christian Cabato <renzcabato@Renzs-Mac-mini.local>
2025-10-06 21:19:50 +0800

    D

--- resources/views/timekeeping/index.blade.php
+++ resources/views/timekeeping/index.blade.php
@@ -213,8 +213,8 @@
                                         @if ($loop->first)
                                             <td rowspan="{{ $rowCount }}">{{ date('M d - l',strtotime($date)) }}</td>
                                         @endif
-                                        <td class="wrap-text" style="word-wrap: break-word; ">{{ $act->project->name }} - {{ $act->task->title }}</td>
-                                        <td class="wrap-text" style="word-wrap: break-word; ">{{ $act->activity }}</td>
+                                        <td class="wrap-text" style="word-wrap: break-word; ">{!! preg_replace("/\s+/", "\n", $act->project->name." - ".$act->task->title)!!}</td>
+                                        <td class="wrap-text" style="word-wrap: break-word; ">{!! preg_replace("/\s+/", "\n", $act->activity)!!}</td>
                                         <td>{{ $act->hours }} hrs</td>
                                     </tr>
                                 @endforeach
