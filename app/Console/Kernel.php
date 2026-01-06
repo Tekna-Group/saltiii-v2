eebc8b21d70b6a59e7452ff02f7f1355140a565c
renzcabato1 <cabato.renz.renz@gmail.com>
2026-01-06 15:32:07 +0800

    Done

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,10 +34,14 @@ class Kernel extends ConsoleKernel
         ->timezone('Asia/Manila')
         ->withoutOverlapping();
         $schedule->call(function () {
-            \App\StripeCustomer::where('status', 'active')
-                ->where('next_billing_date', '<', now())
-                ->update(['status' => 'inactive']);
-        })->everyFiveMinutes();
+            $subs = \App\StripeCustomer::where('status', 'active')->get();
+    
+            foreach ($subs as $sub) {
+                if (Carbon::parse($sub->next_billing_date)->lt(Carbon::now())) {
+                    $sub->update(['status' => 'inactive']);
+                }
+            }
+        })->daily();
     }
 
     /**
